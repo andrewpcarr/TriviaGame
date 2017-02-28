8d8347a6e15de1e3ce8c439f8a5ff9b98b936577
Andrew Carr <andrewpcarr@gmail.com>
2017-02-28 03:12:59 -0600

    Completed game logic. Zero on timer now calculates score and reset button resets the interval properly.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,18 +4,110 @@ var wrong = 0;
 var noResponse = 0;
 
 // Our timer variables
-var count = 61;
+var count = 61; // set at 61 so the user sees 60 displayed initially
 var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
 
-// This gets the clock running
+// This is the logic for the timer
 function timer()
 {
   count = count-1;
-  if (count <= 0)
-  {
+  // If timer reaches 0 before the user finishes, this logic checks their score
+  if (count <= 0) {
      clearInterval(counter);
-     $('.fixed').html('Time\s up!');
-     return;
+     if ($('#correct1').is(':checked')) {
+		correct++;
+	} else if ($('.wrong1').is(':checked')) {
+		wrong++;
+	} else {
+		noResponse++;
+	}
+
+	if ($('#correct2').is(':checked')) {
+		correct++;
+	} else if ($('.wrong2').is(':checked')) {
+		wrong++;
+	} else {
+		noResponse++;
+	}
+
+	if ($('#correct3').is(':checked')) {
+		correct++;
+	} else if ($('.wrong3').is(':checked')) {
+		wrong++;
+	} else {
+		noResponse++;
+	}
+
+	if ($('#correct4').is(':checked')) {
+		correct++;
+	} else if ($('.wrong4').is(':checked')) {
+		wrong++;
+	} else {
+		noResponse++;
+	}
+
+	if ($('#correct5').is(':checked')) {
+		correct++;
+	} else if ($('.wrong5').is(':checked')) {
+		wrong++;
+	} else {
+		noResponse++;
+	}
+
+	if ($('#correct6').is(':checked')) {
+		correct++;
+	} else if ($('.wrong6').is(':checked')) {
+		wrong++;
+	} else {
+		noResponse++;
+	}
+
+	if ($('#correct7').is(':checked')) {
+		correct++;
+	} else if ($('.wrong7').is(':checked')) {
+		wrong++;
+	} else {
+		noResponse++;
+	}
+
+	if ($('#correct8').is(':checked')) {
+		correct++;
+	} else if ($('.wrong8').is(':checked')) {
+		wrong++;
+	} else {
+		noResponse++;
+	}
+
+	if ($('#correct9').is(':checked')) {
+		correct++;
+	} else if ($('.wrong9').is(':checked')) {
+		wrong++;
+	} else {
+		noResponse++;
+	}
+
+	if ($('#correct10').is(':checked')) {
+		correct++;
+	} else if ($('.wrong10').is(':checked')) {
+		wrong++;
+	} else {
+		noResponse++;
+	}
+
+	$('html, body').animate({ scrollTop: 0 }, 'fast');
+
+	$('.questions').css('display', 'none');
+	$('.score').css('display', 'block');
+	$('.fixed').css('display', 'none');
+
+	$('#right').html('Number right: ' + correct);
+	$('#wrong').html('Number wrong: ' + wrong);
+
+	if (noResponse > 0) {
+		$('#noResponse').html('No response: ' + noResponse);
+	} else {
+		$('#noResponse').empty();
+	}
   }
   if (count <= 10) {
   	$('.fixed').css('color', '#c50331');
@@ -26,7 +118,7 @@ function timer()
   $('.fixed').html('Time:' + count);
 }
 
-timer();
+
 
 // Start Game button
 $('.startGame').on('click', function() {
@@ -37,6 +129,7 @@ $('.startGame').on('click', function() {
 	$('.fixed').css('display', 'block');
 	$('.questions').css('display', 'block');
 
+	timer();
 });
 
 // This button tabulates the score and moves to the score screen
@@ -132,7 +225,11 @@ $('.checkScore').on('click', function() {
 
 	if (noResponse > 0) {
 		$('#noResponse').html('No response: ' + noResponse);
-	}	
+	} else {
+		$('#noResponse').empty();
+	}
+
+
 	console.log(correct);
 	console.log(wrong);
 });
@@ -148,5 +245,12 @@ $('.replay').on('click', function() {
 	$('.score').css('display', 'none');
 	$('.fixed').css('display', 'block');
 
+	$('.fixed').css('color', '#ffee83');
+  	$('.fixed').css('font-size', '48px');
+  	$('.fixed').css('border', '2px solid black');
+
 	$('html, body').animate({ scrollTop: 0 }, 'slow');
+	clearInterval(counter);
+	counter = setInterval(timer, 1000);
+	timer();
 });
